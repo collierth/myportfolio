d42e5c3d0d2ac4f41aa38e2afd0a30727c14ff8c
Collier Hawkins <collier_hawkins@bellsouth.net>
2021-12-22 10:34:59 -0500

    Snap-scroll and Beginning Navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,17 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">collier.</a>
+                    <div className="itemContainer">
+                        
+                    </div>
+                </div>
+                <div className="right">
+                    this is right
+                </div>
+            </div>
         </div>
     )
 }
