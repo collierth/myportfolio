d9837dde246b534cfcb90e299d4f46adb67bed0e
Collier Hawkins <collier_hawkins@bellsouth.net>
2021-12-22 16:15:04 -0500

    Intro Text, BG Gradient, and Slide Down Animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,11 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+            <div className="wrapper">
+                <h2>Hello, I'm</h2>
+                <h1>Collier Hawkins</h1>
+                <h3>Computer Science Major at Georgia State University</h3>
+            </div>
         </div>
     )
 }
