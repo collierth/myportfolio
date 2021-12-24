64e37aaa12bace26fac297d927b1360548595e18
Collier Hawkins <collier_hawkins@bellsouth.net>
2021-12-24 13:05:24 -0500

    Finish Skills Section, Begin About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,24 @@
 import "./about.scss"
+import {KeyboardArrowDownSharp, School, Grading} from "@material-ui/icons"
+
 
 export default function About() {
     return (
         <div className="about" id="about">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/image.png" alt=""/>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>I'm a</h2>
+                    <h1>Computer Science Major</h1>
+                    <h2>at Georgia State University<School/></h2>
+                    <h3></h3>
+                    <a href="#skills" className="icon"><KeyboardArrowDownSharp style={{ fontSize: 50 }}/></a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,14 +6,13 @@ export default function Skills() {
     return (
         <div className="skills" id="skills">
             <h1>Skills</h1>
-            <ul>
-                <li className="active">Front-End</li>
-                <li>Back-End</li>
-                <li>Design</li>
-            </ul>
+            <h4>Get to know more about what I do</h4> 
             <div className="container">
                 <div className="item">
                     <h3>Javascript</h3>
+                </div>
+                <div className="item">
+                    <h3>React</h3>
                 </div>
                 <div className="item">
                     <h3>CSS</h3>
@@ -21,15 +20,24 @@ export default function Skills() {
                 <div className="item">
                     <h3>HTML</h3>
                 </div>
-                <div className="item">
-                    <h3>React</h3>
-                </div>
-                <div className="item">
+                <div className="item2">
                     <h3>Bootstrap</h3>
                 </div>
+                <div className="item2">
+                    <h3>Java</h3>
+                </div>
+                <div className="item2">
+                    <h3>MySQL</h3>
+                </div>
+                <div className="item3">
+                    <h3>Photoshop</h3>
+                </div>
+                <div className="item3">
+                    <h3>Premiere Pro</h3>
+                </div>      
             </div>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            <a href="#works" className="icon"><KeyboardArrowDownSharp style={{ fontSize: 100 }}/></a>
+            <a href="#works" className="icon"><KeyboardArrowDownSharp style={{ fontSize: 50 }}/></a>
         </div>
     )
 }
